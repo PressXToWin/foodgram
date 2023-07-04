a0b05742d04ef0505542c2b2ff098cfe3882855a
PressXToWin <PressXToWin@users.noreply.github.com>
2023-07-04 16:50:14 +0700

    modify filters

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -6,7 +6,12 @@ class RecipeFilter(FilterSet):
     is_in_shopping_cart = filters.BooleanFilter(method='filter_is_in_shopping_cart')
 
     def filter_is_favorited(self, queryset, name, value):
-        return queryset.filter(favorites__user=self.request.user)
+        if self.request.user.is_authenticated:
+            return queryset.filter(favorites__user=self.request.user)
+        return queryset.none()
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
-        return queryset.filter(in_cart__user=self.request.user)
+        if self.request.user.is_authenticated:
+            return queryset.filter(in_cart__user=self.request.user)
+        return queryset.none()
+
