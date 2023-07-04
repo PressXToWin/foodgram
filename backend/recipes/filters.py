4b9b4c42901e7e9fd3dfaed9c656edd7a2fc673c
PressXToWin <PressXToWin@users.noreply.github.com>
2023-07-04 16:39:43 +0700

    is_in_cart filter

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -1,7 +1,12 @@
 from django_filters.rest_framework import FilterSet, filters
 
+
 class RecipeFilter(FilterSet):
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(method='filter_is_in_shopping_cart')
 
     def filter_is_favorited(self, queryset, name, value):
         return queryset.filter(favorites__user=self.request.user)
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        return queryset.filter(in_cart__user=self.request.user)
