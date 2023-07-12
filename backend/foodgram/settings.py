ee43116d2b8aaa1c34dc49cb04ef70369843e9b6
PressXToWin <PressXToWin@users.noreply.github.com>
2023-07-12 10:15:20 +0700

    add CSRF_TRUSTED_ORIGINS

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -15,6 +15,8 @@ DEBUG = os.getenv('DEBUG', 'False')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split()
 
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split()
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
