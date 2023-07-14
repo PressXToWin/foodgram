5d72bf41c25778fb7c0f5093b6313f08c336f78d
PressXToWin <PressXToWin@users.noreply.github.com>
2023-07-14 08:21:26 +0700

    admin panel improving

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -3,9 +3,31 @@ from django.contrib import admin
 from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
                             RecipeTag, ShoppingCart, Subscribe, Tag)
 
-admin.site.register(Recipe)
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'name',
+        'author',
+    ]
+    search_fields = ['name', 'author__username']
+    list_filter = ['tags']
+    empty_value_display = '-пусто-'
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'name',
+        'measurement_unit'
+    ]
+    search_fields = ['name']
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Tag)
-admin.site.register(Ingredient)
+admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Subscribe)
 admin.site.register(Favorite)
 admin.site.register(ShoppingCart)
