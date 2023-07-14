de6a0587ebf36135e738abef6143cc19bb2149f0
PressXToWin <PressXToWin@users.noreply.github.com>
2023-07-14 09:05:16 +0700

    add author filter

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -12,6 +12,7 @@ class IngredientFilter(FilterSet):
 
 
 class RecipeFilter(FilterSet):
+    author = filters.CharFilter()
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
         method='filter_is_in_shopping_cart'
